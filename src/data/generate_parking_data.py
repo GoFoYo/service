057d4d50f6f0d1649a56c147f883b972bd1c35e0
Vlad Zusko <vladzvu11@gmail.com>
2024-02-22 23:56:47 +0200

    feat(data): add parking data generating script

---
+++ src/data/generate_parking_data.py
@@ -0,0 +1,20 @@
+import random
+import csv
+
+def generate_parking_row(rows_number: int):
+    rows = []
+
+    for _ in range(rows_number):
+        row = [int(random.uniform(10, 50)), random.uniform(10, 50), random.uniform(10, 50)]
+        rows.append(row)
+
+    return rows
+
+file_path = "./parking.csv"
+rows_number = 1500
+rows = generate_parking_row(rows_number)
+
+
+with open(file_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(rows)
